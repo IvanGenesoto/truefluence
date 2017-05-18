1bb931874df6322a0cfaf7b10d46b8aa2a76a773
John Yamashiro <john.yamashiro@gmail.com>
2017-05-18 09:59:19 -0700

    adding upsert relationship functions #8

--- server/database.js
+++ server/database.js
@@ -26,26 +26,66 @@ Database.prototype.getFollowers = userId => {
   })
 }
 
-Database.prototype.upsertRelationship = (userId, followingId) => {
-  return new Promise((resolve, reject) => {
-    knex('relationships')
-      .count('*')
-      .where('user_id', userId)
-      .andWhere('following_id', followingId)
-        .then(result => {
-          
-        })
-  })
+Database.prototype.createRelationship = (userId, followingId, following) => {
   const timeNow = new Date(Date.now()).toISOString();
   const relationship = {
     user_id: userId,
     following_id: followingId,
     created_at: timeNow,
     updated_at: timeNow,
-    following: true
-  }
+    following: following
+  };
   return knex('relationships')
-    .insert(relationship);
+  .insert(relationship);
+}
+
+Database.prototype.updateRelationship = (userId, followingId, following) => {
+  const timeNow = new Date(Date.now()).toISOString();
+  const relationship = {
+    updated_at: timeNow,
+    following: following
+  };
+  return knex('relationships')
+    .where('user_id', userId)
+    .andWhere('following_id', followingId)
+    .update(relationship);
+}
+
+Database.prototype.upsertRelationship = (userId, followingId, following = true) => {
+  return new Promise((resolve, reject) => {
+    knex('relationships')
+      .count('*')
+      .where('user_id', userId)
+      .andWhere('following_id', followingId)
+        .then(result => {
+          const count = Number(result[0].count);
+          if (count > 0) {
+            this.updateRelationship(userId, followingId, following)
+              .then(result => {
+                console.log('relationship updated for', userId);
+              })
+              .catch(err => {
+                console.log('could not update relationship for', userId);
+              });
+          } else {
+            this.createRelationship(userId, followingId, following)
+              .then(result => {
+                console.log('relationship created for', userId);
+              })
+              .catch(err => {
+                console.log('could not create relationship for', userId);
+              });
+          }
+        })
+  })
+}
+
+Database.prototype.createUser = params => {
+
+}
+
+Database.prototype.updateUser = params => {
+  
 }
 
 Database.prototype.upsertUser = params => {
